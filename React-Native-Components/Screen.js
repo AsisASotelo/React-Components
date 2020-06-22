70e1b5598ece551e46e5859da6692eb4d30e9070
AsisASotelo <asis.a.sotelo@gmail.com>
2020-06-22 16:49:36 -0700

    Fix Screens SafeAreaView

--- React-Native-Components/Screen.js
+++ React-Native-Components/Screen.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 import Constants from 'expo-constants';
-import {StyleSheet, SafeAreaView} from 'react-native';
+import {StyleSheet, SafeAreaView, View} from 'react-native';
 
-function Screen({children}) {
+function Screen({children,style}) {
 
     return(
 
 
-        <SafeAreaView style = {styles.screen}>
-            {children}
+        <SafeAreaView style = {[styles.screen,style]}>
+           <View style={style}>{children}</View> 
         </SafeAreaView>
 
     );
@@ -17,7 +17,8 @@ function Screen({children}) {
 
 const styles = StyleSheet.create({
     screen:{
-        paddingTop: Constants.statusBarHeight 
+        paddingTop: Constants.statusBarHeight,
+        flex:1,
     }
 
 
