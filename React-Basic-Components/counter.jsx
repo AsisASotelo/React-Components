f7b606861f29cf9f205385937b322c2793903142
AsisASotelo <asis.a.sotelo@gmail.com>
2020-07-24 20:54:04 -0700

    Update

--- React-Basic-Components/counter.jsx
+++ React-Basic-Components/counter.jsx
@@ -3,9 +3,8 @@ import React, { Component } from 'react';
 
 class counter extends Component {
     state = {
-        count:0,
-        imageUrl: 'https://picsum.photos/200',
-        tags:['tag1','tag2','tag3']
+        value:this.props.value,
+        
         
     };
     // constructor() {
@@ -28,8 +27,8 @@ class counter extends Component {
     };
 
     handleIncrement=product=> {
-        console.log(product);
-        this.setState({count:this.state.count+1});
+        
+        this.setState({value:this.state.value+1});
     }
 
     
@@ -37,31 +36,35 @@ class counter extends Component {
 
         
         return (
-            <>
+            <div>
+               
 
                 {/* <img src = {this.state.imageUrl} alt="A picture of something"/> */}
                 <span className = {this.getBadgeClass()}>{this.formatCount()}</span>
-                <button onClick = {()=> this.handleIncrement({id:1})}
-                className = "btn btn-secondary btn-sm">Increment</button> 
+                <button 
+                    onClick = {this.handleIncrement}
+                    className = "btn btn-secondary btn-sm">Increment</button> 
+
+                <button onClick = {this.props.onDelete}className="btn btn-danger btn-sm m-2">Delete</button>
 
 
-                {this.renderTags()}
+                {/* {this.renderTags()} */}
                 
 
-            </>
+            </div>
             
         );
     }
 
     getBadgeClass() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.state.value === 0) ? "warning" : "primary";
         return classes;
     }
 
     formatCount(){
-        const {count} =this.state;
-        return count === 0 ? "Zero": count;
+        const {value} =this.state;
+        return value === 0 ? "Zero": value;
     }
 }
 
