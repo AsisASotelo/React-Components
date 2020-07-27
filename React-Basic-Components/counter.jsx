dc47f05e4108d181ea07956256928ef54860214e
AsisASotelo <asis.a.sotelo@gmail.com>
2020-07-26 21:05:07 -0700

    Update Counter.jsx

--- React-Basic-Components/counter.jsx
+++ React-Basic-Components/counter.jsx
@@ -1,36 +1,28 @@
 import React, { Component } from 'react';
 
 
-class counter extends Component {
-    state = {
-        value:this.props.value,
-        
-        
-    };
+class Counter extends Component {
+   
     // constructor() {
     //     super();
     //     this.handleIncrement = this.handleIncrement.bind(this);
     // }
     
 
-    renderTags(){
-        if(this.state.tags.length === 0) return <p> There are no tags!</p>;
+    // renderTags(){
+    //     if(this.state.tags.length === 0) return <p> There are no tags!</p>;
 
-        return <ul>
-        {this.state.tags.map(tag=> <li key={tag}>{tag}</li>)}
-    </ul>
-    }
+    //     return <ul>
+    //     {this.state.tags.map(tag=> <li key={tag}>{tag}</li>)}
+    // </ul>
+    // }
 
     style = {
         fontSize: 10,
         fontWeight:'bold',
     };
 
-    handleIncrement=product=> {
-        
-        this.setState({value:this.state.value+1});
-    }
-
+    
     
     render() {
 
@@ -42,10 +34,10 @@ class counter extends Component {
                 {/* <img src = {this.state.imageUrl} alt="A picture of something"/> */}
                 <span className = {this.getBadgeClass()}>{this.formatCount()}</span>
                 <button 
-                    onClick = {this.handleIncrement}
+                    onClick = {()=> this.props.onIncrement(this.props.counter)}
                     className = "btn btn-secondary btn-sm">Increment</button> 
 
-                <button onClick = {this.props.onDelete}className="btn btn-danger btn-sm m-2">Delete</button>
+                <button onClick = {() =>this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete </button>
 
 
                 {/* {this.renderTags()} */}
@@ -58,14 +50,14 @@ class counter extends Component {
 
     getBadgeClass() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.value === 0) ? "warning" : "primary";
+        classes += (this.props.counter.value === 0) ? "warning" : "primary";
         return classes;
     }
 
     formatCount(){
-        const {value} =this.state;
+        const {value} =this.props.counter;
         return value === 0 ? "Zero": value;
     }
 }
 
-export default counter;
+export default Counter;
